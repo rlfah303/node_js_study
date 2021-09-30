10df4875efa9bd60c9415b7e5019f41615d0524b
rlfah303 <rlarlfah303@gmail.com>
2021-09-30 19:54:33 +0900

    render HTML

--- express_crash_course/server.js
+++ express_crash_course/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app =express()
 
+
+app.set("view engine",'ejs') 
+
 app.get('/',(req,res)=>{
-    console.log("Here")
-    res.download("server.js")
+    console.log("Here")  
+    res.render("index",{text: "World"})
+
     // res.status(500).send("hi")
 
 })
-app.listen(3000)
+app.listen(3000) 
