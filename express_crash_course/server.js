d8cafd2a4220eb54bf1464a7183004b75a8a3776
rlfah303 <rlarlfah303@gmail.com>
2021-09-30 19:35:11 +0900

    send data

---
+++ express_crash_course/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app =express()
+
+app.get('/',(req,res)=>{
+    console.log("Here")
+    res.download("server.js")
+    // res.status(500).send("hi")
+
+})
+app.listen(3000)
