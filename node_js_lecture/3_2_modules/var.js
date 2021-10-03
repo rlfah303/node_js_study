4af8f10ae5bc032f58419fa600494305926495da
rlfah303 <rlarlfah303@gmail.com>
2021-10-03 19:52:59 +0900

    util functions

--- node_js_lecture/3_2_modules/var.js
+++ node_js_lecture/3_2_modules/var.js
@@ -2,3 +2,7 @@ const odd ='홀수';
 const even ='짝수';
 
 module.exports ={odd, even}
+
+
+// exports.odd. = odd;
+// exports.even = even;
---
+++ node_js_lecture/3_9_util.js
@@ -0,0 +1,18 @@
+const util = require('util');
+const crypto = require('crypto');
+
+const donTuseMe = util.deprecate((x, y)=>{
+    console.log(x + y);
+}, " 이 함수는 deprecated 되었으니 사용 ㄴㄴ");
+donTuseMe(1,2);
+
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+
+randomBytesPromise(64)
+    .then((buf) => {
+        console.log(buf.toString('base64'));
+    })
+    .catch((error)=>{
+        console.error(error);
+    });
