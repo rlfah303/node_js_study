a54a3c49a8668cc9a1143e39b7961ae4f639b02b
rlfah303 <rlarlfah303@gmail.com>
2021-10-07 23:44:34 +0900

    worker_threads

--- node_js_lecture/3_9_util.js
+++ node_js_lecture/3_9_util.js
@@ -1,14 +1,14 @@
 const util = require('util');
 const crypto = require('crypto');
 
+//deprecated
 const donTuseMe = util.deprecate((x, y)=>{
     console.log(x + y);
 }, " 이 함수는 deprecated 되었으니 사용 ㄴㄴ");
 donTuseMe(1,2);
 
-
+//promisify (callback 을 promise로 바꾸기)
 const randomBytesPromise = util.promisify(crypto.randomBytes);
-
 randomBytesPromise(64)
     .then((buf) => {
         console.log(buf.toString('base64'));
