8ead701fed1d654c644fe6481245b1f412aa82ab
rlfah303 <rlarlfah303@gmail.com>
2021-10-12 22:12:11 +0900

    write file

--- node_js_lecture/3_12_fs/readFile.js
+++ node_js_lecture/3_12_fs/readFile.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-fs.readFile('./readme.txt', (err,data)=>{
-    if(err){
+
+fs.readFile('./readme.txt')
+    .then((data) => {
+        console.log(data);
+        console.log(data.toString());
+    })
+    .catch((err)=>{
         throw err;
-    }
-    console.log(data);
-    console.log(data.toString());
 });
+
+// const fs = require('fs');
+
+// fs.readFile('./readme.txt', (err,data)=>{
+//     if(err){
+//         throw err;
+//     }
+//     console.log(data);
+//     console.log(data.toString());
+// });
