64cce1a73fa3e211db4ed9235403f8ddebfa136b
rlfah303 <rlarlfah303@gmail.com>
2021-10-13 19:07:52 +0900

    buffer practice

--- node_js_lecture/3_12_fs/writeFIle.js
+++ node_js_lecture/3_12_fs/writeFIle.js
@@ -1,6 +1,5 @@
 const fs = require('fs').promises;
 
-
 fs.writeFile('./writeme.txt',"글이 입력됩니다")
     .then(() => {
         return fs.readFile('./writeme.txt');
---
+++ node_js_lecture/3_13_buffer.js
@@ -0,0 +1,4 @@
+const buffer = Buffer.from('저를 버퍼로 바꿔보세요');
+console.log(buffer);
+console.log(buffer.toString());
+ 
