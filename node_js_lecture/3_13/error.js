2cfc8bf2a8ee8cc64e6e18aa9d05792da5815380
rlfah303 <rlarlfah303@gmail.com>
2021-10-17 23:37:48 +0900

    error together

--- node_js_lecture/3_13/error.js
+++ node_js_lecture/3_13/error.js
@@ -11,13 +11,30 @@
 // },1000);
 
 //콜백 함수에서 에러 객체 제공
-const fs= require('fs');
+// const fs= require('fs');
+
+// setInterval(()=>{
+//     fs.unlink('./abcd.js',(err)=>{
+//         if (err){
+//             console.error(err);
+//         }
+//     });
+    
+// },1000);
+
+
+//모든에러 처리
+//콜백 함수의 동작 보장이안됨
+//에러 기록용
+
+process.on('uncaughtException',(err)=>{
+    console.error('예기치못한 에러', err);
+});
 
 setInterval(()=>{
-    fs.unlink('./abcd.js',(err)=>{
-        if (err){
-            console.error(err);
-        }
-    });
-    
+    throw new Error('서버를 고장내주마');
 },1000);
+
+setTimeout(()=>{
+    console.log('실행됩니다');
+},2000);
