27877807cb2d78424f8fb6218e7b4c0acd631491
rlfah303 <rlarlfah303@gmail.com>
2021-10-17 21:39:00 +0900

    callback error

--- node_js_lecture/3_13/error.js
+++ node_js_lecture/3_13/error.js
@@ -1,8 +1,23 @@
+// error 잡는 경우들
+
+//에러가 발생할 곳은 try catch로 감쌈
+// setInterval(()=>{
+//     console.log('시작');
+//     try{
+//         throw new Error('고장내기');
+//     } catch(err){
+//         console.error(err);
+//     }
+// },1000);
+
+//콜백 함수에서 에러 객체 제공
+const fs= require('fs');
+
 setInterval(()=>{
-    console.log('시작');
-    try{
-        throw new Error('고장내기');
-    } catch(err){
-        console.error(err);
-    }
+    fs.unlink('./abcd.js',(err)=>{
+        if (err){
+            console.error(err);
+        }
+    });
+    
 },1000);
