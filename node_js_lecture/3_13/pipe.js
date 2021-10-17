30221293a3e729dc103cb95e0c4410997a71bb82
rlfah303 <rlarlfah303@gmail.com>
2021-10-17 20:39:29 +0900

    stream with pipe

---
+++ node_js_lecture/3_13/pipe.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const zlib = require('zlib');
+
+const readStream = fs.createReadStream('./readme.txt', { highWaterMark: 16 });
+const zilbStream= zlib.createGzip();
+const writeStream = fs.createWriteStream('./writeme.txt.gz');
+readStream.pipe(zilbStream).pipe(writeStream);
