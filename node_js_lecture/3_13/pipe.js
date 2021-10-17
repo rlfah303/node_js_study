cab606a408c7d16fd4ab45a0128132d52d9159af
rlfah303 <rlarlfah303@gmail.com>
2021-10-17 21:17:58 +0900

    threadpool with crypto

--- node_js_lecture/3_13/pipe.js
+++ node_js_lecture/3_13/pipe.js
@@ -5,3 +5,6 @@ const readStream = fs.createReadStream('./readme.txt', { highWaterMark: 16 });
 const zilbStream= zlib.createGzip();
 const writeStream = fs.createWriteStream('./writeme.txt.gz');
 readStream.pipe(zilbStream).pipe(writeStream);
+
+
+
