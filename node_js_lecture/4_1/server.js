7f5895e1073908e082d21d78cf2918f9b60b3d8a
rlfah303 <rlarlfah303@gmail.com>
2021-10-29 16:44:20 +0900

    handle error

--- node_js_lecture/4_1/server.js
+++ node_js_lecture/4_1/server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
-http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{
     res.write('<h1>Hello Node<h1>')
 })
 
     .listen(8080,()=>{
         console.log('8080번 포트에서 대기중')
-    })
+});
 
-    
+server.on('error', (error)=>{
+    console.error(error);
+});
+
