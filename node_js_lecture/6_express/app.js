a6457fefb08f75cd8626dcc25f8facd0dcb35a0e
rlfah303 <rlarlfah303@gmail.com>
2021-11-03 17:20:18 +0900

    listen to port 3000

---
+++ node_js_lecture/6_express/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// port 를 속성/변수로 지정
+app.set('port', process.env.PORT || 3000);
+
+app.get('/',(req,res)=>{
+    res.send("hello express");
+});
+
+app.listen(app.get('port'),()=>{
+    console.log('express 실행')
+});
