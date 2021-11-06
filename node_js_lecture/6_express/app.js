11c73ed95f61c2e270d29b70d0293cc94f6f9772
rlfah303 <rlarlfah303@gmail.com>
2021-11-06 19:10:19 +0900

    add a middleware

--- node_js_lecture/6_express/app.js
+++ node_js_lecture/6_express/app.js
@@ -3,12 +3,20 @@ const path = require('path');
 
 const app = express();
 
+app.use((req,res,next)=>{
+    console.log("모든요청에 실행하고싶어요");
+    next();
+})
 // port 를 속성/변수로 지정
 app.set('port', process.env.PORT || 3000);
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
     //res.sendFile('./index.html');
+});
+
+app.post('/',(req,res)=>{
+    res.send('hello express');
 });
 
 app.listen(app.get('port'), () => {
